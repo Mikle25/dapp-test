1ed50b8583593d4b1e57b12f9c7049d62b44f4e1
mda-25 <m.dashutin@everstake.one>
2023-08-01 13:35:09 +0300

    fix: show network

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import MetamaskConnection from "./ConnectWallet";
 import { useProvider } from "./hooks/useProvider";
 import { BalanceDisplay } from "./components/balance/BalanceDisplay";
-import { ProviderContextProvider } from "./store";
 
 const App = () => {
   const { currentNetwork } = useProvider();
 
   return (
     <>
-      <ProviderContextProvider>
-        <h1>Dapp network: {currentNetwork}</h1>
-        <MetamaskConnection />
-        <BalanceDisplay />
-      </ProviderContextProvider>
+      <h1>Dapp network: {currentNetwork}</h1>
+      <MetamaskConnection />
+      <BalanceDisplay />
     </>
   );
 };
